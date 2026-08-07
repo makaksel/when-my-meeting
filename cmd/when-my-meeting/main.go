9b9ff30e812edfa58ba9facb1ea3a1ebc75b5958
Alex Makarov <makarovav@answer-42.ru>
2026-08-07 17:22:23 +0700

    add cmd/main and split service into two part: init and run

---
+++ cmd/when-my-meeting/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+	"makaksel/when-my-meeting/internal/app"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+	defer cancel()
+
+	a, err := app.New()
+	if err != nil {
+		log.Fatalf("app init: %v", err)
+	}
+
+	if err := a.Run(ctx); err != nil {
+		log.Fatalf("app run: %v", err)
+	}
+}
