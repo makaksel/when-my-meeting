7885c5869de0f980c0d4cdefe6dd9a90d01dd35b
Alex Makarov <makarovav@answer-42.ru>
2026-08-21 06:29:14 +0700

    feat: add tray menu

---
+++ internal/gui/tray/tray.go
@@ -0,0 +1,134 @@
+package tray
+
+import (
+	_ "embed"
+	"sync"
+	"time"
+
+	"context"
+	"fmt"
+	"makaksel/when-my-meeting/internal/assets"
+	"makaksel/when-my-meeting/internal/config"
+	"makaksel/when-my-meeting/internal/state"
+
+	"fyne.io/systray"
+)
+
+type Service struct {
+	Config *config.Service
+	State  *state.Service
+
+	cancel       context.CancelFunc
+	handleSync   func() error
+	openSettings func()
+
+	refreshMu sync.Mutex
+}
+
+func New(
+	cfg *config.Service,
+	s *state.Service,
+	cancel context.CancelFunc,
+	handleSync func() error,
+	openSettings func(),
+) *Service {
+
+	return &Service{
+		Config:       cfg,
+		State:        s,
+		cancel:       cancel,
+		handleSync:   handleSync,
+		refreshMu:    sync.Mutex{},
+		openSettings: openSettings,
+	}
+}
+
+func (s *Service) Run(ctx context.Context) (
+	start func(),
+	end func(),
+) {
+	go s.listenState(ctx)
+
+	return systray.RunWithExternalLoop(s.onReady, s.onExit)
+}
+
+func (s *Service) onReady() {
+	systray.SetIcon(assets.Icon)
+
+	s.refreshMenu()
+}
+
+func (s *Service) refreshMenu() {
+	s.refreshMu.Lock()
+	defer s.refreshMu.Unlock()
+
+	s.updateTitle()
+	systray.ResetMenu()
+	s.updateMenu()
+}
+
+func (s *Service) updateTitle() {
+	n := s.State.GetNextMeeting()
+
+	if n == nil || n.Start == nil {
+		systray.SetTitle("")
+		systray.SetTooltip("Сегодня нет встреч")
+		return
+	}
+
+	t := fmt.Sprintf("%s", n.Start.Local().Format("15:04"))
+
+	systray.SetTitle(t)
+	systray.SetTooltip(fmt.Sprintf("%s %s", t, n.Title))
+}
+
+func (s *Service) updateMenu() {
+	n := s.State.GetNextMeeting()
+	if n != nil && n.Start != nil {
+		s.addMeetingItem(n)
+		systray.AddSeparator() // ------------
+	}
+
+	meetings := s.State.GetFolowingMeetings()
+	if len(meetings) > 1 {
+		for _, m := range meetings[1:] {
+			s.addMeetingItem(&m)
+		}
+		systray.AddSeparator() // ------------
+	}
+
+	s.addBtn("Обновить 🔄", "Обновить", func() {
+		s.handleSync()
+
+	})
+
+	s.addBtn("Настройки", "Настройки", func() {
+		s.openSettings()
+	})
+
+	s.addBtn("Выйти", "Выйти", func() {
+		s.onExit()
+	})
+}
+
+func (s *Service) onExit() {
+	now := time.Now()
+	fmt.Println("Exit at", now.String())
+
+	systray.Quit()
+	s.cancel()
+}
+
+func (s *Service) listenState(ctx context.Context) {
+	changes := s.State.Changes()
+
+	for {
+		select {
+		case <-changes:
+			s.refreshMenu()
+
+		case <-ctx.Done():
+			return
+		}
+	}
+}
