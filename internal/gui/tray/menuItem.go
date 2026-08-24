c1a2ca23029ff1b30f2a821cdb2810274ec7bc37
Alex Makarov <makarovav@answer-42.ru>
2026-08-25 06:26:53 +0700

    feat: add tray menu item

---
+++ internal/gui/tray/menuItem.go
@@ -0,0 +1,30 @@
+package tray
+
+import (
+	"fmt"
+	"makaksel/when-my-meeting/internal/domain"
+	"makaksel/when-my-meeting/internal/utils"
+
+	"fyne.io/systray"
+)
+
+func (s *Service) addBtn(title, tooltip string, callback func()) *systray.MenuItem {
+	btn := systray.AddMenuItem(title, tooltip)
+	go func() {
+		for range btn.ClickedCh {
+			callback()
+		}
+	}()
+
+	return btn
+}
+
+func (s *Service) addMeetingItem(m *domain.Meeting) {
+	title := fmt.Sprintf("%s-%s %s", m.Start.Local().Format("15:04"), m.End.Local().Format("15:04"), m.Title)
+
+	s.addBtn(title, title, func() {
+		if m.Location != "" {
+			utils.OpenBrowser(m.Location)
+		}
+	})
+}
